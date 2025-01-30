0e38f644d179eb0d5f15d7f835499b19ab28c869
akbaralinurbhanej <akbaralinurbhanej99@gmail.com>
2025-01-30 10:50:35 +0530

    remove

--- chatbot.py
+++ chatbot.py
@@ -23,11 +23,7 @@ if "messages" not in st.session_state.keys(): # Initialize the chat message hist
 # llm = ChatOpenAI(model_name="gpt-4o-mini")
 # llm = ChatGoogleGenerativeAI(model = "gemini-pro")
 llm = ChatOpenAI(model = "meta-llama/Llama-3.2-90B-Vision-Instruct-Turbo",
-<<<<<<< HEAD
                       openai_api_key = st.secrets["TOGETHER_API_KEY"] , ## use your key
-=======
-                      openai_api_key = st.secrets["74bb604a9b53804a4870a362f010d3714a373dc6e5b26eeb5dcba0ccfa4e723d"] , ## use your key
->>>>>>> 967616c (add api)
                       openai_api_base = "https://api.together.xyz/v1"
 
 )
@@ -38,6 +34,8 @@ conversation = ConversationChain(memory=st.session_state.buffer_memory, llm=llm)
 st.title("🗣️ Conversational Chatbot")
 st.subheader("㈻ Simple Chat Interface for LLMs by Build Fast with AI")
 
+# Add GitHub repository link
+st.markdown("[View on GitHub](https://github.com/YourUsername/YourRepoName)")
 
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
